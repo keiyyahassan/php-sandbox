ccc6806a3c48d3aecbfd2232184f6e9a9ef1c6e9
Keiyya <keiyyaalex@gmail.com>
2024-10-31 15:57:26 +0300

    String concatenation

--- index.php
+++ index.php
@@ -1,45 +1,35 @@
 <?php
+$firstName = 'John';
+$lastName = 'Doe';
 
-// String
-$name = 'Keiyya Alex';
-$name2 = "John Doe";
+$fullName = $firstName . " " . $lastName
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-var_dump($name);
-echo '<br/>';
-echo getType($name2);
-echo '<br/>';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Learn PHP From Scratch</title>
+</head>
 
-// Integer
-$age = 35;
-var_dump($age);
-echo '<br/>';
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <!-- Output -->
+            <?= 'Hello, my name is ' . $fullName . '<br/>' ?>
+            <?= "Hello, my name is $fullName" . '<br/>' ?>
+            <?= 'Hello, my name is \'John\'' . '<br/>' ?>
+            <?= "Hello, my name is \"John\"" . '<br/>' ?>
+        </div>
+    </div>
+</body>
 
-// Float
-$rating = 4.5;
-var_dump($rating);
-echo '<br/>';
-
-// Boolaen
-$isLoaded = true;
-var_dump($isLoaded);
-echo '<br/>';
-
-// Array
-$friends = ['John', 'Jack', "Jane"];
-var_dump($friends);
-echo '<br/>';
-
-// Object
-$person = new stdClass();
-var_dump($person);
-echo '<br/>';
-
-// Null
-$car = null;
-var_dump($car);
-echo '<br/>';
-
-// Resource
-$file = fopen('sample.txt', 'r');
-var_dump($file);
+</html>
