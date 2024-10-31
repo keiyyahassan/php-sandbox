9c645f65bd3dcb360e84b901d096e92757f5d004
Keiyya <keiyyaalex@gmail.com>
2024-10-31 14:20:51 +0300

    Data Types

--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <?php
-$title = 'Learn PHP From Scratch';
-$heading = 'Welcome to the course';
-$body = "In this course, you will learn the fundamentals of the PHP language";
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+// String
+$name = 'Keiyya Alex';
+$name2 = "John Doe";
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title><?= $title ?></title>
-</head>
 
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold"><?= $title ?></h1>
-        </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
-            <p><?= $body ?></p>
-        </div>
-    </div>
-</body>
+var_dump($name);
+echo '<br/>';
+echo getType($name2);
+echo '<br/>';
 
-</html>
+// Integer
+$age = 35;
+var_dump($age);
+echo '<br/>';
+
+// Float
+$rating = 4.5;
+var_dump($rating);
+echo '<br/>';
+
+// Boolaen
+$isLoaded = true;
+var_dump($isLoaded);
+echo '<br/>';
+
+// Array
+$friends = ['John', 'Jack', "Jane"];
+var_dump($friends);
+echo '<br/>';
+
+// Object
+$person = new stdClass();
+var_dump($person);
+echo '<br/>';
+
+// Null
+$car = null;
+var_dump($car);
+echo '<br/>';
+
+// Resource
+$file = fopen('sample.txt', 'r');
+var_dump($file);
