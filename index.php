6491887122778df830ac51a197548cbf9736f1a2
Keiyya <keiyyaalex@gmail.com>
2024-11-01 09:40:43 +0300

    Arithmetic operators & Functions

--- index.php
+++ index.php
@@ -1,32 +1,96 @@
 <?php
-$title = "Introduction to PHP";
-$pageTitle = "Brad's PHP Blog ";
-$author = "John Doe";
-$body = " PHP (Hypertext Preprocessor) is a widely used server-side scripting
-            language that has revolutionized web development. With its simplicity,
-            flexibility, and vast community support, PHP has become the backbone of
-            countless dynamic websites and web applications."
+/*
+| Arithmetic Operators
+| Operator | Description    |
+| -------- | -------------- |
+| `+`      | Addition       |
+| `-`      | Subtraction    |
+| `*`      | Multiplication |
+| `/`      | Division       |
+| `%`      | Modulus        |
+*/
+
+$output = null;
+$num1 = 20;
+$num2 = 10;
+
+// Basic Math
+$output = "$num1 + $num2 =" . $num1 + $num2;
+$output = "$num1 - $num2 =" . $num1 - $num2;
+$output = "$num1 * $num2 =" . $num1 * $num2;
+$output = "$num1 / $num2 =" . $num1 / $num2;
+$output = "$num1 % $num2 =" . $num1 % $num2;
+
+// Assignment Operator
+$num3 = 10;
+// $num3 = $num3 + 20;
+// $num3 += 20;
+// $num3 -= 20;
+$num3 /= 2;
+
+// $output = $num3;
+
+// Built-in PHP Functions
+//rand()
+
+$output = rand();
+$output = getrandmax();
+$output = rand(1, 10);
+
+// round()
+$output = round(4.7);
+
+// round up
+$output = ceil(4.2);
+
+// round down
+$output = floor(4.2);
+
+// square root
+$output = sqrt(4);
+
+// pi
+$output = pi();
+
+// absolute
+$output = abs(-4.7);
+
+//max
+$output = max(1, 2, 3, 50);
+$output = max([1, 2, 35, 3,]);
+
+//min
+$output = min(1, 2, 3, 50);
+$output = min([10, 2, 35, 7,]);
+
+$output = number_format(12345678.191234, 2, '.', ',');
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $pageTitle . " | " . $title ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
 </head>
 
-<body>
-    <main>
-        <h1><?= $title ?></title>
-        </h1>
-        <p>By: <?= $author ?></p>
-        <p>
-            <?= $body ?>
-        </p>
-    </main>
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <!-- Output -->
+            <p class="text-xl">
+                <?= $output ?>
+            </p>
+        </div>
+    </div>
 </body>
 
 </html>
