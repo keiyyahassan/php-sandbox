b0ca1754f67e30943f578eb1e7642b12d4242113
Keiyya <keiyyaalex@gmail.com>
2024-11-02 06:08:17 +0300

    Array Functions

--- 02-arrays-and-iteration/02-array-functions/index.php
+++ 02-arrays-and-iteration/02-array-functions/index.php
@@ -1,5 +1,63 @@
 <?php
 $output = null;
+
+$ids = [10, 22, 15, 45, 67];
+$users = ['user2', 'user3', 'user1'];
+
+// Count
+$output = count($ids);
+
+// Sort
+sort($ids);
+sort($users);
+
+// Reverse sort
+rsort($ids);
+rsort($users);
+
+// Add at the end of an array
+array_push($ids, 100);
+array_push($users, 'user4');
+
+// Remove last item from array
+array_pop($ids);
+array_pop($users);
+
+// Remove first item from array
+array_shift($ids);
+array_shift($users);
+
+// Add at the begining of an array
+array_unshift($ids, 100);
+array_unshift($users, 'user3');
+
+// Slice
+$ids2 = array_slice($ids, 2, 3);
+// var_dump($ids2)
+
+// Splice
+array_splice($ids, 1, 1, 'New ID');
+array_splice($users, 0, 1, 'New User');
+
+// Array sum
+$output = 'Sum of IDs: ' . array_sum($ids);
+
+// Search
+$output = 'User 2 is at index: ' . array_search('user2', $users);
+
+array_push($users, 'user3');
+
+// in_array
+$output = 'User 3 exists: ' . in_array('user3', $users);
+
+// Explode
+$tags = 'tech,code,programming';
+$tagsArr = explode(',', $tags);
+// var_dump($tagsArr)
+
+// Implode
+$output = implode(", ", $users)
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +80,17 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">IDs Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($ids); ?>
+                </pre>
+            </p>
+            <h2 class="text-xl font-semibold my-4">Users Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($users); ?>
+                </pre>
             </p>
         </div>
     </div>
