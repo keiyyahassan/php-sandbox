2dde2a67f3917ca991774b7a82582399ae71aef9
Keiyya <keiyyaalex@gmail.com>
2024-11-02 06:56:16 +0300

    Associative arrays

--- 02-arrays-and-iteration/03-associative-arrays/index.php
+++ 02-arrays-and-iteration/03-associative-arrays/index.php
@@ -1,5 +1,21 @@
 <?php
 $output = null;
+
+$user = [
+    'name' => 'John',
+    'email' => 'john@gmail.com',
+    'password' => '123456',
+    'hobbies' => ['Tennis', 'Video Games']
+];
+
+$output = $user['name'];
+$output = $user['email'];
+
+$output = $user['hobbies'][0];
+
+$user['address'] = '123 Main St';
+
+unset($user['address']);
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +38,12 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">User Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($user) ?>
+                </pre>
+            </p>
         </div>
     </div>
 </body>
